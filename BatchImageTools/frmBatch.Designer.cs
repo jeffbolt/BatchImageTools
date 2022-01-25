08862629df1fdd6af50f4951511ecc9c14fa6797
jeffbolt <jeffbolt@users.noreply.github.com>
2022-01-25 13:21:35 -0600

    Add project files.

---
+++ BatchImageTools/frmBatch.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace BatchImageTools
+{
+	partial class frmBatch
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.lblSelect = new System.Windows.Forms.Label();
+			this.lvwImages = new System.Windows.Forms.ListView();
+			this.lblConvertTo = new System.Windows.Forms.Label();
+			this.cmbFormat = new System.Windows.Forms.ComboBox();
+			this.Path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Height = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Width = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.btnConvert = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitle.Location = new System.Drawing.Point(13, 13);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(184, 21);
+			this.lblTitle.TabIndex = 0;
+			this.lblTitle.Text = "Batch Image Converter";
+			// 
+			// lblSelect
+			// 
+			this.lblSelect.AutoSize = true;
+			this.lblSelect.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSelect.Location = new System.Drawing.Point(14, 44);
+			this.lblSelect.Name = "lblSelect";
+			this.lblSelect.Size = new System.Drawing.Size(165, 15);
+			this.lblSelect.TabIndex = 1;
+			this.lblSelect.Text = "Drag or browse for image files";
+			// 
+			// lvwImages
+			// 
+			this.lvwImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lvwImages.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Path,
+            this.Height,
+            this.Width,
+            this.Size});
+			this.lvwImages.FullRowSelect = true;
+			this.lvwImages.HideSelection = false;
+			this.lvwImages.Location = new System.Drawing.Point(17, 63);
+			this.lvwImages.Name = "lvwImages";
+			this.lvwImages.Size = new System.Drawing.Size(771, 341);
+			this.lvwImages.TabIndex = 2;
+			this.lvwImages.UseCompatibleStateImageBehavior = false;
+			this.lvwImages.View = System.Windows.Forms.View.Details;
+			// 
+			// lblConvertTo
+			// 
+			this.lblConvertTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.lblConvertTo.AutoSize = true;
+			this.lblConvertTo.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblConvertTo.Location = new System.Drawing.Point(14, 417);
+			this.lblConvertTo.Name = "lblConvertTo";
+			this.lblConvertTo.Size = new System.Drawing.Size(66, 15);
+			this.lblConvertTo.TabIndex = 3;
+			this.lblConvertTo.Text = "Convert to:";
+			// 
+			// cmbFormat
+			// 
+			this.cmbFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.cmbFormat.AutoCompleteCustomSource.AddRange(new string[] {
+            "PNG",
+            "JPG",
+            "BMP"});
+			this.cmbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbFormat.FormattingEnabled = true;
+			this.cmbFormat.Items.AddRange(new object[] {
+            "PNG - Portable Network Graphics (*.png)"});
+			this.cmbFormat.Location = new System.Drawing.Point(86, 415);
+			this.cmbFormat.Name = "cmbFormat";
+			this.cmbFormat.Size = new System.Drawing.Size(289, 21);
+			this.cmbFormat.TabIndex = 4;
+			// 
+			// Path
+			// 
+			this.Path.Text = "File Path";
+			this.Path.Width = 400;
+			// 
+			// Size
+			// 
+			this.Size.Text = "Size";
+			this.Size.Width = 100;
+			// 
+			// Height
+			// 
+			this.Height.Text = "Height";
+			this.Height.Width = 100;
+			// 
+			// Width
+			// 
+			this.Width.Text = "Width";
+			this.Width.Width = 100;
+			// 
+			// btnConvert
+			// 
+			this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnConvert.Location = new System.Drawing.Point(381, 413);
+			this.btnConvert.Name = "btnConvert";
+			this.btnConvert.Size = new System.Drawing.Size(75, 25);
+			this.btnConvert.TabIndex = 5;
+			this.btnConvert.Text = "Convert";
+			this.btnConvert.UseVisualStyleBackColor = true;
+			this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+			// 
+			// frmBatch
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnConvert);
+			this.Controls.Add(this.cmbFormat);
+			this.Controls.Add(this.lblConvertTo);
+			this.Controls.Add(this.lvwImages);
+			this.Controls.Add(this.lblSelect);
+			this.Controls.Add(this.lblTitle);
+			this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "frmBatch";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Batch Image Converter";
+			this.Load += new System.EventHandler(this.frmBatch_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblTitle;
+		private System.Windows.Forms.Label lblSelect;
+		private System.Windows.Forms.ListView lvwImages;
+		private System.Windows.Forms.Label lblConvertTo;
+		private System.Windows.Forms.ComboBox cmbFormat;
+		private System.Windows.Forms.ColumnHeader Path;
+		private System.Windows.Forms.ColumnHeader Height;
+		private System.Windows.Forms.ColumnHeader Width;
+		private System.Windows.Forms.ColumnHeader Size;
+		private System.Windows.Forms.Button btnConvert;
+	}
+}
+
---
+++ BatchImageTools/frmBatch.cs
@@ -0,0 +1,261 @@
+﻿using Svg;
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BatchImageTools
+{
+	public partial class frmBatch : Form
+	{
+		public frmBatch()
+		{
+			InitializeComponent();
+
+			// Enable drag & drop https://stackoverflow.com/questions/68598/how-do-i-drag-and-drop-files-into-an-application
+			this.AllowDrop = true;
+			this.DragEnter += new DragEventHandler(frmBatch_DragEnter);
+			this.DragDrop += new DragEventHandler(frmBatch_DragDrop);
+		}
+
+		private void frmBatch_Load(object sender, EventArgs e)
+		{
+			// TODO: Resize column widths
+
+			// Load convert to formats
+			cmbFormat.Items.Clear();
+			cmbFormat.Items.Add("PNG - Portable Network Graphics (*.png)");
+			cmbFormat.SelectedIndex = 0;
+			
+			//cmbFormat.Items.Add(new ListViewItem
+			//{
+			//	Tag = "PNG",
+			//	Text = "PNG - Portable Network Graphics (*.png)"
+			//});
+		}
+
+		void frmBatch_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+		}
+
+		private void frmBatch_DragDrop(object sender, DragEventArgs e)
+		{
+			// https://stackoverflow.com/questions/8550937/c-sharp-drag-and-drop-files-to-form
+			// https://stackoverflow.com/questions/729090/c-how-to-add-subitems-in-listview
+			// https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
+			// https://www.c-sharpcorner.com/uploadfile/mahesh/understanding-message-box-in-windows-forms-using-C-Sharp/
+			try
+			{
+				if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				{
+					string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+					foreach (string filePath in filePaths)
+					{
+						if (File.Exists(filePath))
+						{
+							var fi = new FileInfo(filePath);
+
+							switch (fi.Extension.ToLower())
+							{
+								case ".svg":
+									// https://stackoverflow.com/questions/58910/
+									try
+									{
+										var svg = SvgDocument.Open(filePath);
+										if (svg != null)
+										{
+											var item = new ListViewItem
+											{
+												Text = filePath
+											};
+											item.SubItems.Add(svg.Height.ToString() + " px");
+											item.SubItems.Add(svg.Width.ToString() + " px");
+											item.SubItems.Add(GetFileSizeSuffix(fi.Length));
+											lvwImages.Items.Add(item);
+										}
+									}
+									catch
+									{
+										string message = string.Format("Could not read SVG image information for {0}.", filePath);
+										MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+									}
+									break;
+								default:
+									try
+									{
+										var bmp = new Bitmap(filePath);
+										if (bmp != null)
+										{
+											var item = new ListViewItem
+											{
+												Text = filePath
+											};
+											item.SubItems.Add(bmp.Height.ToString("N0") + " px");
+											item.SubItems.Add(bmp.Width.ToString("N0") + " px");
+											item.SubItems.Add(GetFileSizeSuffix(fi.Length));
+											lvwImages.Items.Add(item);
+										}
+									}
+									catch
+									{
+										string message = string.Format("Could not read bitmap image information for {0}.", filePath);
+										MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+									}
+									break;
+							}
+						}
+
+						// Code to read the contents of the text file
+						//if (File.Exists(fileLoc))
+						//	using (TextReader tr = new StreamReader(fileLoc))
+						//	{
+						//		Console.WriteLine(tr.ReadToEnd());
+						//	}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+
+		private string GetFileSizeSuffix(long value, int decimalPlaces = 1)
+		{
+			// https://stackoverflow.com/questions/14488796/does-net-provide-an-easy-way-convert-bytes-to-kb-mb-gb-etc#14488941
+			string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+			if (decimalPlaces < 0) throw new ArgumentOutOfRangeException("decimalPlaces");
+
+			if (value < 0) return "-" + GetFileSizeSuffix(-value, decimalPlaces);
+			if (value == 0) return string.Format("{0:n" + decimalPlaces + "} bytes", 0);
+
+			// mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+			int mag = (int)Math.Log(value, 1024);
+
+			// 1L << (mag * 10) == 2 ^ (10 * mag) 
+			// [i.e. the number of bytes in the unit corresponding to mag]
+			decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+			// make adjustment when the value is large enough that
+			// it would round up to 1000 or more
+			if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+			{
+				mag += 1;
+				adjustedSize /= 1024;
+			}
+
+			return string.Format("{0:n" + decimalPlaces + "} {1}", adjustedSize, SizeSuffixes[mag]);
+		}
+
+		private bool SvgToPng(string inputPath)
+		{
+			// https://github.com/svg-net/SVG/blob/master/Samples/SVGViewer/SvgViewer.cs
+			// https://stackoverflow.com/questions/58910/converting-svg-to-png-using-c-sharp#12884409
+			try
+			{
+				SvgDocument svgDoc = SvgDocument.Open(inputPath);
+
+				if (svgDoc != null)
+				{
+					var fi = new FileInfo(inputPath);
+					string outputPath = string.Concat(inputPath.TrimEnd(fi.Extension.ToCharArray()), ".png");
+					using (var bitmap = svgDoc.Draw())
+					{
+						// TOOD: Fix poor quality image with codecs
+						var imageCodecs = ImageCodecInfo.GetImageEncoders();
+						ImageCodecInfo codecInfo = imageCodecs.FirstOrDefault(x => x.FormatDescription == "PNG");
+						var parameters = new EncoderParameters(1);
+						/*
+							ChrominanceTable	f2e455dc-09b3-4316-8260-676ada32481c
+							ColorDepth			66087055-ad66-4c7c-9a18-38a2310b8337
+							Compression			e09d739d-ccd4-44ee-8eba-3fbf8be4fc58
+							LuminanceTable		edb33bce-0266-4a77-b904-27216099e717
+							Quality				1d5be4b5-fa4a-452d-9cdd-5db35105e7eb
+							RenderMethod		6d42c53a-229a-4825-8bb7-5c99e2b9a8b8
+							SaveFlag			292266fc-ac40-47bf-8cfc-a85b89a655de
+							ScanMethod			3a4e2661-3109-4e56-8536-42c156e7dcfa
+							Transformation		8d0eb2d1-a58e-4ea8-aa14-108074b7b6f9
+							Version				24d18c76-814a-41a4-bf53-1c219cccf797
+						*/
+
+						var compressionEncoder = new System.Drawing.Imaging.Encoder(new Guid("e09d739d-ccd4-44ee-8eba-3fbf8be4fc58"));
+						parameters.Param[0] = new EncoderParameter(compressionEncoder, (long)10);  // set to 10% ?
+						
+						bitmap?.Save(outputPath, codecInfo, parameters);
+					}
+				}
+
+				//string xml = svgDoc.GetXML();
+				//var byteArray = Encoding.ASCII.GetBytes(svgDoc.Content.ToCharArray());
+				//using (var stream = new MemoryStream(byteArray))
+				//{
+				//	var svgDocument = SvgDocument.Open(path);
+				//	var bitmap = svgDocument.Draw();
+				//	bitmap.Save(path, ImageFormat.Png);
+				//}
+
+				//var svgDoc = SvgDocument.Open(path);
+				//var svgFileContents = svgDoc.ToString();
+				//var byteArray = Encoding.ASCII.GetBytes(svgFileContents);
+				//using (var stream = new MemoryStream(byteArray))
+				//{
+				//	var svgDocument = SvgDocument.Open(path);
+				//	var bitmap = svgDocument.Draw();
+				//	bitmap.Save(path, ImageFormat.Png);
+				//}
+				//var svgFileContents = svgDoc.ToString();
+				//var byteArray = Encoding.ASCII.GetBytes(svgFileContents);
+				//using (var stream = new MemoryStream(byteArray))
+				//{
+				//	var svgDocument = SvgDocument.Open(stream);
+				//	var bitmap = svgDocument.Draw();
+				//	bitmap.Save(path, ImageFormat.Png);
+				//}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+
+			return false;
+		}
+
+		private void btnConvert_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				foreach (ListViewItem itm in lvwImages.SelectedItems)
+				{
+					string filePath = itm.Text;
+					if (File.Exists(filePath))
+					{
+						var fi = new FileInfo(filePath);
+
+						switch (fi.Extension)
+						{
+							case ".svg":
+								switch (cmbFormat.SelectedItem.ToString().Substring(0, 3))
+								{
+									case "PNG":
+										SvgToPng(filePath);
+									break;
+								}
+								break;
+						}
+
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+	}
+}
